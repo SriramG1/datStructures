b237fd9f801cceae260f02cdf0cbce57dbf3167c
SriramG1 <sriramhht@gmail.com>
2022-03-31 01:31:12 +0530

    Binary search using recursion

--- src/search/BinarySearch.java
+++ src/search/BinarySearch.java
@@ -1,28 +1,32 @@
 package search;
 
 public class BinarySearch {
-    private void search(int[] array,int number){
-        int min=0;
-        int max= array.length;
-        while(min<max){
-            int mid=(min+max)/2;
+    private int search(int[] array,int first,int last,int number){
+        if(last>=first){
+            int mid=(first+last)/2;
             if(array[mid]==number){
-                System.out.println("Your position is : "+mid);
-                break;
+                return mid;
             }
-            else if(array[mid]<number){
-                min=mid+1;
+            if(array[mid]<number){
+                return search(array,mid+1,last,number);
             }
-            else{
-                max=mid-1;
+            if(array[mid]>number){
+                return search(array,first,mid-1,number);
             }
         }
+        return -1;
     }
     public static void main(String[] args) {
         //Use in sorted array
         BinarySearch binarySearch = new BinarySearch();
         int[] array={1,2,3,4,5,6,7,8,9};
-        int number=6;
-        binarySearch.search(array,number);
+        int number=9;
+        int result=binarySearch.search(array,0, array.length-1,number);
+        if(result==-1){
+            System.out.println("Your value is not in array");
+        }
+        else {
+            System.out.println("Index of your number is "+result);
+        }
     }
 }
